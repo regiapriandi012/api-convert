93be52cebecb0be6a46e81c50a4b06cd7b74ea0c
Regi Apriandi <69528812+regiapriandi012@users.noreply.github.com>
2022-11-26 23:05:08 +0700

    Update init.sql

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,15 +1,17 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY '123456';
+--CREATE USER 'auth_user'@'localhost' IDENTIFIED BY '123456';
 
-CREATE DATABASE auth_db;
+--CREATE DATABASE auth_db;
 
-GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_user'@'localhost';
+--GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_user'@'localhost';
 
-use auth_db;
+--use auth_db;
 
+--Create Database User
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR (255) NOT NULL,
     password VARCHAR (255) NOT NULL
 );
 
-INSERT INTO user (email, password) VALUES ('regiapriandi012@gmail.com', '123456');
+--Add Username and Password for User
+INSERT INTO user (email, password) VALUES ('you@gmail.com', '123456');
